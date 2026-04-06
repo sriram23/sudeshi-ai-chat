d1f03628c5009cd1883732e7d3e994049cc0a910
Sriram <sriramsubramanian23@gmail.com>
2026-04-06 11:58:01 +0530

    Fixed issue with model type.

--- app/features/chat/components/ChatInput.tsx
+++ app/features/chat/components/ChatInput.tsx
@@ -1,6 +1,6 @@
 import { Send, Square } from "lucide-react";
 
-const ChatInput = ({ input, setInput, settings, setSettings, status, sendMessage, stopStreaming }:{ input: string; setInput: (value: string) => void; settings: { model: string }; setSettings: (newSettings: { model: string }) => void; status: string; sendMessage: (value: string, model: string) => void; stopStreaming: () => void }) => {
+const ChatInput = ({ input, setInput, settings, setSettings, status, sendMessage, stopStreaming }:{ input: string; setInput: (value: string) => void; settings: { model: "sarvam-30b" | "sarvam-105b" }; setSettings: (newSettings: { model: "sarvam-30b" | "sarvam-105b" }) => void; status: string; sendMessage: (value: string, model: string) => void; stopStreaming: () => void }) => {
     const handleInputHeight = (e: React.FormEvent<HTMLTextAreaElement>) => {
         const textarea = e.currentTarget;
         textarea.style.height = "auto";
@@ -15,7 +15,7 @@ const ChatInput = ({ input, setInput, settings, setSettings, status, sendMessage
             )}
             <textarea onInput={handleInputHeight} value={input} onChange={(e) => setInput(e.target.value)} rows={1} className="no-scrollbar focus-visible:ring-2 focus:outline-none focus-visible:ring-blue-500 mt-auto min-h-10 max-h-32 h-full p-2 border rounded resize-none" placeholder="Type your message..."/>
             <div className="absolute right-2 bottom-1">
-                <select onChange={(e) => setSettings({ model: e.target.value })} value={settings?.model} className="mr-2 p-1 border rounded">
+                <select onChange={(e) => setSettings({ model: e.target.value as "sarvam-30b" | "sarvam-105b" })} value={settings?.model} className="mr-2 p-1 border rounded">
                     <option value="sarvam-30b">Sarvam 30B</option>
                     <option value="sarvam-105b">Sarvam 105B</option>
                 </select>
