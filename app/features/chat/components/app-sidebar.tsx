833e1958c3f2422c7c0d66a571efe2ecb96618f8
Sriram <sriramsubramanian23@gmail.com>
2026-04-14 13:15:37 +0530

    Fixed a aesthetics in the sidebar.

--- app/features/chat/components/app-sidebar.tsx
+++ app/features/chat/components/app-sidebar.tsx
@@ -59,119 +59,122 @@ export function AppSidebar(): React.ReactNode {
     }
   };
   return (
-    <Sidebar collapsible="icon" className="bg-gradient-to-br from-zinc-50 to-zinc-100 dark:from-zinc-950 dark:to-zinc-900">
-      <SidebarHeader />
-      <SidebarContent>
-        <SidebarGroup title="Welcome to Sudeshi" >
-          <SidebarGroupLabel className="text-3xl">Sudeshi</SidebarGroupLabel>
-          <SidebarGroupLabel className="text-sm text-gray-500">Your personal AI assistant</SidebarGroupLabel>
-          <SidebarGroupLabel className="text-sm">Powered by Sarvam AI</SidebarGroupLabel>
-        </SidebarGroup>
-        <SidebarGroup title="New Chat" >
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarMenuButton disabled={status === "streaming"} onClick={() => createNewConversation()} className="bg-zinc-900 rounded text-white h-10"><Plus />New Conversation</SidebarMenuButton>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarGroup>
-        <SidebarGroup title="Conversations">
-          <SidebarMenu>
-          {conversations.map(conv => (
-            <SidebarMenuItem key={conv.id}>
-              <SidebarMenuButton title={conv.title} disabled={status === "streaming"} onClick={() => setActiveConversation(conv.id)} className={`p-2 m-1 cursor-pointer rounded-lg ${conv.id === activeConversationId ? 'bg-zinc-900 text-white' : 'bg-gray-200 dark:bg-zinc-700'}`}>
-                <SidebarGroupLabel className="text-sm">{conv.title.length > 18 ? conv.title.slice(0, 18) + "..." : conv.title}</SidebarGroupLabel>
-                <div className="flex-1 justify-end flex gap-2 text-transparent hover:text-zinc-900 hover:dark:text-white">
-                  <DropdownMenu>
-                    <DropdownMenuTrigger nativeButton={false} render={<div onClick={(e) => e.stopPropagation()} role="presentation"/>}><Ellipsis className="text-white"/></DropdownMenuTrigger>
-                    <DropdownMenuContent className="bg-white dark:bg-zinc-900">
-                      <DropdownMenuGroup>
-                        <DropdownMenuItem className="hover:bg-yellow-300" onClick={(e) => {
-                          setNewName(conv.title);
-                          setEditDialogId(conv.id);
-                          e.stopPropagation();
-                        }}><Pencil/> Rename</DropdownMenuItem>
-                        <DropdownMenuItem className="hover:bg-red-500" onClick={(e) => {
-                          setDeleteDialogId(conv.id);
-                          e.stopPropagation();
-                        }}><Trash/> Delete Chat</DropdownMenuItem>
-                      </DropdownMenuGroup>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                  {/* <span onClick={(e) => handleEdit(e, conv.id)} className="cursor-pointer rounded border border-transparent hover:border-white hover:bg-white hover:dark:bg-zinc-950 p-1"><Pencil /></span>
-                  <span onClick={(e) => handleDelete(e, conv.id)} className="cursor-pointer rounded border border-transparent hover:border-white hover:bg-white hover:dark:bg-zinc-950 p-1"><Trash /></span> */}
-                </div>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          ))}
-          </SidebarMenu>
-        </SidebarGroup>
-      </SidebarContent>
-      {deleteDialogId ? (
-        <AlertDialog
-          open={Boolean(deleteDialogId)}
-          onOpenChange={(open) => {
-            if (!open) setDeleteDialogId(null)
-          }}
-        >
-          <AlertDialogContent className="bg-gradient-to-br from-zinc-50 to-zinc-100 dark:from-zinc-950 dark:to-zinc-900">
-            <AlertDialogHeader>
-              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-              <AlertDialogDescription>
-                This action cannot be undone. This will permanently delete your chat.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction onClick={() => deleteDialogId && handleDelete(deleteDialogId)} className="px-4 py-2 text-sm font-medium text-white bg-zinc-600 border border-transparent rounded-md hover:bg-zinc-700">Continue</AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
-      ) : null}
-      {editDialogId ? (
-        <Dialog
-          open={Boolean(editDialogId)}
-          onOpenChange={(open) => {
-            if (!open) {
-              setEditDialogId(null);
-              setNewName("");
-            }
-          }}
-        >
-          <DialogContent className="bg-gradient-to-br from-zinc-50 to-zinc-100 dark:from-zinc-950 dark:to-zinc-900">
-            <DialogHeader>
-              <DialogTitle>Rename Chat</DialogTitle>
-            </DialogHeader>
-            <div className="space-y-2">
-              <label htmlFor="chat-title" className="text-sm font-medium">New Chat Title</label>
-              <input
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-                type="text"
-                id="chat-title"
-                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-zinc-900"
-                placeholder="Enter new chat title"
-              />
-            </div>
-            <DialogFooter>
-              <DialogClose render={
-                <button aria-label="Close Dialog" className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50">Cancel</button>
-              } />
-              <button
-                aria-label="Rename Chat"
-                onClick={() => editDialogId && handleEdit(editDialogId)}
-                className="px-4 py-2 text-sm font-medium text-white bg-zinc-600 border border-transparent rounded-md hover:bg-zinc-700"
-              >
-                Rename
-              </button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-      ) : null}
-      <SidebarFooter>
-        <div>
-          <button aria-label="Theme toggle" className="border p-1 border-zinc-900 dark:border-zinc-600 rounded" onClick={() => setTheme(theme === "dark"?"light":"dark")}>{theme === "dark" ? <Sun /> : <Moon />} </button>
-        </div>
-      </SidebarFooter>
-    </Sidebar>
+    // <div className="bg-gradient-to-br  from-orange-400 via-white to-green-700 dark:from-zinc-950 dark:via-zinc-900 dark:to-zinc-900">
+      <Sidebar collapsible="icon" className="bg-white/10 backdrop-blur-lg dark:bg-transparent dark:backdrop-blur-none border-zinc-300">
+        <SidebarHeader>
+          <SidebarContent>
+            <SidebarGroup title="Welcome to Sudeshi" >
+            <SidebarGroupLabel className="text-3xl">Sudeshi</SidebarGroupLabel>
+            <SidebarGroupLabel className="text-sm text-zinc-600">Your personal AI assistant</SidebarGroupLabel>
+            <SidebarGroupLabel className="text-sm">Powered by Sarvam AI</SidebarGroupLabel>
+          </SidebarGroup>
+          </SidebarContent>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup title="New Chat" >
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton disabled={status === "streaming"} onClick={() => createNewConversation()} className="bg-zinc-700 rounded text-white h-10"><Plus />New Conversation</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroup>
+          <SidebarGroup title="Conversations">
+            <SidebarMenu>
+            {conversations.map(conv => (
+              <SidebarMenuItem key={conv.id}>
+                <SidebarMenuButton title={conv.title} disabled={status === "streaming"} onClick={() => setActiveConversation(conv.id)} className={`p-2 m-1 cursor-pointer rounded-lg ${conv.id === activeConversationId ? 'bg-zinc-700 text-white' : 'bg-zinc-100 dark:bg-zinc-900'}`}>
+                  <SidebarGroupLabel className="text-sm">{conv.title.length > 18 ? conv.title.slice(0, 17) + "..." : conv.title}</SidebarGroupLabel>
+                  <div className="flex-1 justify-end flex gap-2 text-transparent hover:text-zinc-900 hover:dark:text-white">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger nativeButton={false} render={<div onClick={(e) => e.stopPropagation()} role="presentation"/>}><Ellipsis className={`${conv.id === activeConversationId ? 'text-white' : 'text-zinc-950 dark:text-white'}`}/></DropdownMenuTrigger>
+                      <DropdownMenuContent className="bg-white dark:bg-zinc-900">
+                        <DropdownMenuGroup>
+                          <DropdownMenuItem className="hover:bg-yellow-300" onClick={(e) => {
+                            setNewName(conv.title);
+                            setEditDialogId(conv.id);
+                            e.stopPropagation();
+                          }}><Pencil/> Rename</DropdownMenuItem>
+                          <DropdownMenuItem className="hover:bg-red-500" onClick={(e) => {
+                            setDeleteDialogId(conv.id);
+                            e.stopPropagation();
+                          }}><Trash/> Delete Chat</DropdownMenuItem>
+                        </DropdownMenuGroup>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+            </SidebarMenu>
+          </SidebarGroup>
+        </SidebarContent>
+        {deleteDialogId ? (
+          <AlertDialog
+            open={Boolean(deleteDialogId)}
+            onOpenChange={(open) => {
+              if (!open) setDeleteDialogId(null)
+            }}
+          >
+            <AlertDialogContent className="bg-gradient-to-br from-zinc-50 to-zinc-100 dark:from-zinc-950 dark:to-zinc-900">
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete your chat.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={() => deleteDialogId && handleDelete(deleteDialogId)} className="px-4 py-2 text-sm font-medium text-white bg-zinc-600 border border-transparent rounded-md hover:bg-zinc-700">Continue</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        ) : null}
+        {editDialogId ? (
+          <Dialog
+            open={Boolean(editDialogId)}
+            onOpenChange={(open) => {
+              if (!open) {
+                setEditDialogId(null);
+                setNewName("");
+              }
+            }}
+          >
+            <DialogContent className="bg-gradient-to-br from-zinc-50 to-zinc-100 dark:from-zinc-950 dark:to-zinc-900">
+              <DialogHeader>
+                <DialogTitle>Rename Chat</DialogTitle>
+              </DialogHeader>
+              <div className="space-y-2">
+                <label htmlFor="chat-title" className="text-sm font-medium">New Chat Title</label>
+                <input
+                  value={newName}
+                  onChange={(e) => setNewName(e.target.value)}
+                  type="text"
+                  id="chat-title"
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-zinc-900"
+                  placeholder="Enter new chat title"
+                />
+              </div>
+              <DialogFooter>
+                <DialogClose render={
+                  <button aria-label="Close Dialog" className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50">Cancel</button>
+                } />
+                <button
+                  aria-label="Rename Chat"
+                  onClick={() => editDialogId && handleEdit(editDialogId)}
+                  className="px-4 py-2 text-sm font-medium text-white bg-zinc-600 border border-transparent rounded-md hover:bg-zinc-700"
+                >
+                  Rename
+                </button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        ) : null}
+        <SidebarFooter>
+          <div>
+            <button aria-label="Theme toggle" className="border p-1 border-zinc-900 dark:border-zinc-600 rounded" onClick={() => setTheme(theme === "dark"?"light":"dark")}>{theme === "dark" ? <Sun /> : <Moon />} </button>
+          </div>
+        </SidebarFooter>
+      </Sidebar>
+    // </div>
   )
 }
