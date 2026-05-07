623eb4f639d02f3bdd0b35df3b793bd420c8d8da
Sriram <sriramsubramanian23@gmail.com>
2026-05-07 21:03:47 +0530

    URL sanitization to prevent SSRF.

--- app/api/checkOllama/route.ts
+++ app/api/checkOllama/route.ts
@@ -3,10 +3,19 @@ export async function POST(req: NextRequest) {
     const {endpoint} = await req.json();
     if(endpoint) {
         try {
-            const response = await fetch(`${endpoint}/api/tags`, {
+            const url =  new URL("/api/tags", endpoint)
+            if(url.protocol !== "https:" && url.hostname !== "localhost"){
+                throw new Error("Only HTTPS endpoints are allowed")
+            }
+            if(!["443", "80", "11434", "3000", "8080"].includes(url.port) ){
+                throw new Error("Current port usage is restricted")
+            }
+            const response = await fetch(url, {
                 headers: {
                     "Content-Type": "application/json",
-                }
+                },
+                redirect: "error",
+                signal: AbortSignal.timeout(10000)
             });
             const data = await response.json();
             return new Response(JSON.stringify(data), {
